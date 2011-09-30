cc22c852949c9c4ea2146eabf53333cd08bedd41
Sean Burke <seanb@eshark.net>
2011-09-30 12:54:30 -0400

    Fix twigview class

--- src/twigview.php
+++ src/twigview.php
@@ -5,12 +5,13 @@ class TwigView {
     private $_file;
     private $twig;
     public static function init($name=null) {
-        return new Twig($name);
+        Twig_Autoloader::register();
+        return new TwigView($name);
     }
     public function __construct($name=null) {        
         if($name) {            
             $this->load($name);
-        }                
+        }                        
     }
     public function load($name) {
         $path=Config::get('path');
@@ -19,16 +20,15 @@ class TwigView {
             throw new Exception("No valid template supplied");
         }
         $loader=new Twig_Loader_Filesystem($path.'/views/');
-        $this->twig=new Twig_Enviroment($loader);
-        $this->_file=$view;
+        $this->twig=new Twig_Environment($loader);
+        $this->_file=$name;
     }
-    public function bind($key, &$value) {
-        $this->_data[$key]=&$value;
-        //print "Bound $key to ".$value."<br>";
+    public function bind($key, $value) {
+        $this->_data[$key]=$value;
         return $this;
     }
-    public function render() {
-        $this->twig->render($this->_file, $this->_data);
+    public function render() {        
+        print $this->twig->render($this->_file, $this->_data);
     }
 }
 ?>
